fd13b625eb9bdfeecca443ea908e08ee18f1f1bb
amrolim <pg38332@alunos.uminho.pt>
2019-11-03 17:02:19 +0100

    ok ok

--- COMMON_PRE.m
+++ COMMON_PRE.m
@@ -1,7 +1,7 @@
 function [fire, transition] = COMMON_PRE(transition)
 
 switch transition.name
-    case 'tQualityTester' % tQualityTester adds colors "AQuality" and
+    case 'tMCQualityTester' % tQualityTester adds colors "AQuality" and
         %"BQuality" to tokens
         %put here condition to assign randomly a colour to the tokens (see
         %pages until page 26
@@ -26,52 +26,171 @@ switch transition.name
         %given by 
         %AQuality=1-selected__errorperct
         % BQuality=selected__errorperct
-    case 'tTransferDispose'
+    case 'tMCTransferDispose'
             % From pQualityTested, tTransferDispose takes only the token with color 'BQuality'
-            tokID10 = tokenAnyColor('pQualityTested',1, {'BQuality'});%select token with color 'BQuality'
-            fire = tokID10; % fire only if the token has color 'BQuality'
-    case 'tTransferAQ'
-        % From pS, t2 takes only the token with color 't2'
-        tokID11 = tokenAnyColor('pQualityTested',1,{'AQuality'});% select token with color 'AQuality'
-        fire = tokID11; % fire only if the token has color 't2'
+            tokID1 = tokenAnyColor('pMCQualityTested',1, {'BQuality'});%select token with color 'BQuality'
+            fire = tokID1; % fire only if the token has color 'BQuality'
+    case 'tMCTransferAQ'
+        % From pMCQualityTested, tMCTransferAQ takes only the token 
+        %with color 'AQuality'
+        tokID2 = tokenAnyColor('pMCQualityTested',1,{'AQuality'});
+        % select token with color 'AQuality'
+        fire = tokID2; % fire only if the token has color 't2'
 
-    case 'tLamination' % 'tLamination' selects AQuality tokens only from pLamination 
-        tokID1 = tokenAnyColor ('pLamination', 1, {'AQuality'});
-        transition.selected_tokens = tokID1; % tokens to be removed
-        fire = tokID1; % fire ONLY if P1 found
-    case 'tPanelCutting' % 'tLamination' selects AQuality tokens only from pPanelCutting 
-        tokID2 = tokenAnyColor ('pPanelCutting', 1, {'AQuality'});
-        transition.selected_tokens = tokID2; % tokens to be removed
-        fire = tokID2; % fire ONLY if P1 found
-    case 'tPrinting' % 'tPritting' selects AQuality tokens only from pPrintting 
-        tokID3 = tokenAnyColor ('pPrinting', 1, {'AQuality'});
-        transition.selected_tokens = tokID3; % tokens to be removed
-        fire = tokID3; % fire ONLY if P1 found
-    case 'tSorttingMatching' % 'tSorttingMatching' selects AQuality tokens only from pSorttingMatching 
-        tokID4 = tokenAnyColor ('pSorttingMatching', 1, {'AQuality'});
-        transition.selected_tokens = tokID4; % tokens to be removed
-        fire = tokID4; % fire ONLY if P1 found
-    case 'tBladerWeight' % 'tBladerWeight' selects AQuality tokens only from pBladerWeight 
-        tokID5 = tokenAnyColor ('pBladerWeight', 1, {'AQuality'});
-        transition.selected_tokens = tokID5; % tokens to be removed
-        fire = tokID5; % fire ONLY if P1 found
-    case 'tStiching' % 'tStiching' selects AQuality tokens only from pStiching 
-        tokID6 = tokenAnyColor ('pStiching', 1, {'AQuality'});
-        transition.selected_tokens = tokID6; % tokens to be removed
-        fire = tokID6; % fire ONLY if P1 found
-    case 'tQualityAsurance' % 'tQualityAsurance' selects AQuality tokens only from pQualityAsurance 
-        tokID7 = tokenAnyColor ('pStiching', 1, {'AQuality'});
-        transition.selected_tokens = tokID7; % tokens to be removed
-        fire = tokID7; % fire ONLY if P1 found
-    case 'tWashing' % 'tWashing' selects AQuality tokens only from pWashing 
-        tokID8 = tokenAnyColor ('pWashing', 1, {'AQuality'});
-        transition.selected_tokens = tokID8; % tokens to be removed
-        fire = tokID8; % fire ONLY if P1 found
-    case 'tPacking' % 'tPacking' selects AQuality tokens only from pPacking 
-        tokID9 = tokenAnyColor ('pPacking', 1, {'AQuality'});
-        transition.selected_tokens = tokID9; % tokens to be removed
-        fire = tokID9; % fire ONLY if P1 found
+    case 'tLQualityTester' % 'tLQualityTester' adds colors "AQuality" and
+        %"BQuality" to token
+        % 'AQuality' has 95% chance, whereas 'BQuality' has only 5%
+        random_num = rand; % 0 to 1
+        %%%% AQuality has 95% chance, whereas BQuality has only 5%
+        if and(ge(random_num,0), lt(random_num,0.95))
+            color = 'AQuality'; 
+        else
+            color = 'BQuality';
+        end
         
+        transition.new_color = color;
+        transition.override = 1;
+        fire = 1; % always fire
+        
+    case 'tLTransferDispose'
+            % From pLQualityTested, tTransferDispose 
+            %takes only the token with color 'BQuality'
+            tokID3 = tokenAnyColor('pLQualityTested',1, {'BQuality'});
+            %select token with color 'BQuality'
+            fire = tokID3; % fire only if the token has color 'BQuality'
+    case 'tLTransferAQ'
+        % From pLQualityTested, tLTransferAQ takes only the token with 
+        %color 'AQuality'
+        tokID4 = tokenAnyColor('pLQualityTested',1,{'AQuality'});
+        % select token with color 'AQuality'
+        fire = tokID4; % fire only if the token has color 'AQuality'
+        
+    case 'tPCQualityTester' % 'tPCQualityTester' adds colors "AQuality" and
+        %"BQuality" to token
+        % 'AQuality' has 95% chance, whereas 'BQuality' has only 5%
+        random_num = rand; % 0 to 1
+        %%%% AQuality has 95% chance, whereas BQuality has only 5%
+        if and(ge(random_num,0), lt(random_num,0.95))
+            color = 'AQuality'; 
+        else
+            color = 'BQuality';
+        end
+        transition.new_color = color;
+        transition.override = 1;
+        fire = 1; % always fire
+        
+    case 'tPCTransferDispose'
+            % From pPCQualityTested, tPCTransferDispose 
+            %takes only the token with color 'BQuality'
+            tokID5 = tokenAnyColor('pPCQualityTested',1, {'BQuality'});
+            %select token with color 'BQuality'
+            fire = tokID5; % fire only if the token has color 'BQuality'
+    case 'tPCTransferAQ'
+        % From pPCQualityTested, tPCTransferAQ takes only the token with 
+        %color 'AQuality'
+        tokID6 = tokenAnyColor('pPCQualityTested',1,{'AQuality'});
+        % select token with color 'AQuality'
+        fire = tokID6; % fire only if the token has color 'AQuality'
+    
+    case 'tPQualityTester' % 'tPQualityTester' adds colors "AQuality" and
+        %"BQuality" to token
+        % 'AQuality' has 95% chance, whereas 'BQuality' has only 5%
+        random_num = rand; % 0 to 1
+        %%%% t1 has 95% chance, whereas t2 has only 10%
+        if and(ge(random_num,0), lt(random_num,0.95))
+            color = 'AQuality'; 
+        else
+            color = 'BQuality';
+        end
+        transition.new_color = color;
+        transition.override = 1;
+        fire = 1; % always fire
+        
+    case 'tPTransferDispose'
+            % From pPQualityTested, tPTransferDispose 
+            %takes only the token with color 'BQuality'
+            tokID7 = tokenAnyColor('pPQualityTested',1, {'BQuality'});
+            %select token with color 'BQuality'
+            fire = tokID7; % fire only if the token has color 'BQuality'
+    case 'tPTransferAQ'
+        % From pPQualityTested, tPTransferAQ takes only the token with 
+        %color 'AQuality'
+        tokID8 = tokenAnyColor('pPQualityTested',1,{'AQuality'});
+        % select token with color 'AQuality'
+        fire = tokID8; % fire only if the token has color 'AQuality'
+    
+    case 'tSMQualityTester' % 'tSMQualityTester' adds colors "AQuality" and
+        %"BQuality" to token
+        % 'AQuality' has 95% chance, whereas 'BQuality' has only 5%
+        random_num = rand; % 0 to 1
+        %%%% AQuality has 95% chance, whereas BQuality has only 5%
+        if and(ge(random_num,0), lt(random_num,0.95))
+            color = 'AQuality'; 
+        else
+            color = 'BQuality';
+        end
+        transition.new_color = color;
+        transition.override = 1;
+        fire = 1; % always fire
+        
+    case 'tSMTransferDispose'
+            % From pSMQualityTested, tSMTransferDispose 
+            %takes only the token with color 'BQuality'
+            tokID9 = tokenAnyColor('pSMQualityTested',1, {'BQuality'});
+            %select token with color 'BQuality'
+            fire = tokID9; % fire only if the token has color 'BQuality'
+    case 'tSMTransferAQ'
+        % From pSMQualityTested, tSMTransferAQ takes only the token with 
+        %color 'AQuality'
+        tokID10 = tokenAnyColor('pSMQualityTested',1,{'AQuality'});
+        % select token with color 'AQuality'
+        fire = tokID10; % fire only if the token has color 'AQuality'
+    
+    case 'tBWQualityTester' % 'tBWQualityTester' adds colors "AQuality" and
+        %"BQuality" to token
+        % 'AQuality' has 95% chance, whereas 'BQuality' has only 5%
+        random_num = rand; % 0 to 1
+        %%%% t1 has 95% chance, whereas t2 has only 5%
+        if and(ge(random_num,0), lt(random_num,0.95))
+            color = 'AQuality'; 
+        else
+            color = 'BQuality';
+        end
+        transition.new_color = color;
+        transition.override = 1;
+        fire = 1; % always fire
+        
+    case 'tBWTransferDispose'
+            % From pBWQualityTested, tBWTransferDispose 
+            %takes only the token with color 'BQuality'
+            tokID11 = tokenAnyColor('pBWQualityTested',1, {'BQuality'});
+            %select token with color 'BQuality'
+            fire = tokID11; % fire only if the token has color 'BQuality'
+    case 'tBWTransferAQ'
+        % From pBWQualityTested, tBWTransferAQ takes only the token with 
+        %color 'AQuality'
+        tokID12 = tokenAnyColor('pBWQualityTested',1,{'AQuality'});
+        % select token with color 'AQuality'
+        fire = tokID12; % fire only if the token has color 'AQuality'
+        
+        
+%     case 'tStiching' % 'tStiching' selects AQuality tokens only from pStiching 
+%         tokID6 = tokenAnyColor ('pStiching', 1, {'AQuality'});
+%         transition.selected_tokens = tokID6; % tokens to be removed
+%         fire = tokID6; % fire ONLY if P1 found
+%     case 'tQualityAsurance' % 'tQualityAsurance' selects AQuality tokens only from pQualityAsurance 
+%         tokID7 = tokenAnyColor ('pStiching', 1, {'AQuality'});
+%         transition.selected_tokens = tokID7; % tokens to be removed
+%         fire = tokID7; % fire ONLY if P1 found
+%     case 'tWashing' % 'tWashing' selects AQuality tokens only from pWashing 
+%         tokID8 = tokenAnyColor ('pWashing', 1, {'AQuality'});
+%         transition.selected_tokens = tokID8; % tokens to be removed
+%         fire = tokID8; % fire ONLY if P1 found
+%     case 'tPacking' % 'tPacking' selects AQuality tokens only from pPacking 
+%         tokID9 = tokenAnyColor ('pPacking', 1, {'AQuality'});
+%         transition.selected_tokens = tokID9; % tokens to be removed
+%         fire = tokID9; % fire ONLY if P1 found
+%         
     otherwise 
         % just fire
         disp(transition.name)
